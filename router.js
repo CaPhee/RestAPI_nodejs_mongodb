80ef10ff9db854644bce534ba361d380f960d6a6
unknown <quy.nguyen@smartdev.vn>
2017-09-21 06:13:12 +0700

    commit-master

--- router.js
+++ router.js
@@ -1,5 +1,5 @@
 module.exports = function(app) {
-  var todoList = require('../controllers/todoListController');
+  var todoList = require('/controllers/todoListController');
 
   app.route('/tasks')
     .get(todoList.list_all_tasks)
